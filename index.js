121d8a3f23176e61e39174928e576259aa8d35cc
Terrance A. Snyder <terrance.a.snyder@gmail.com>
2015-05-31 14:14:29 -0400

    [ini] update for constructor

--- index.js
+++ index.js
@@ -1,36 +1,16 @@
-var reader = exports;
-
 var fs = require('fs');
 var _ = require('underscore');
 var ini = require('ini');
 
-reader.open = function(options) {
-  options = _.defaults(options || {}, {
-    file: 'C:/strsystm/data/STORE.ini',
-    encoding: 'utf-8'
-  })
-
-  // parse store ini file for configuration
-  // of this system (and also grab common configuration)
-  // TODO store ini should try environment variable first
-  var ini_object = ini.parse(fs.readFileSync(options.file, options.encoding));
-
-  return {
-    get: function(property) {
-      return new section(getIniValueIgnoringCase(ini_object, property));
-    }
-  };
-};
-
 var section = function(obj) {
   return {
     value: function(property) {
-      return getIniValueIgnoringCase(obj, property);
+      return ini_get_string_value(obj, property);
     }
   };
 };
 
-var getIniValueIgnoringCase = function(config, property) {
+var ini_get_string_value = function(config, property) {
   var result = null;
   _.each(_.keys(config), function(key) {
     if (key.toLowerCase().trim() == property.toLowerCase().trim()) {
@@ -38,4 +18,26 @@ var getIniValueIgnoringCase = function(config, property) {
     }
   });
   return result;
+};
+
+exports.open = function(options) {
+  if (_.isString(options)) {
+    options = {
+      file: options,
+      encoding: 'utf-8'
+    };
+  } else {
+    options = _.defaults(options || {}, {
+      file: 'my.ini',
+      encoding: 'utf-8'
+    });
+  }
+
+  var ini_object = ini.parse(fs.readFileSync(options.file, options.encoding));
+
+  return {
+    get: function(property) {
+      return new section(ini_get_string_value(ini_object, property));
+    }
+  };
 };
